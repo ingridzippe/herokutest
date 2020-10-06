644f1f2ed544047117384257751b8c5e05e808f8
Ingrid Zippe <ingridzippe@Ingrids-MacBook-Pro.local>
2020-10-06 00:07:57 -0400

    hello

--- index.js
+++ index.js
@@ -6,5 +6,26 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'hbs')
-  .get('/', (req, res) => res.send('home'))
+  .get('/', (req, res) => res.render('home'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+
+// index.use(bodyParser.urlencoded({extended: true}));
+// index.use(bodyParser.json());
+
+// index.use(express.static(path.join(__dirname, "/public")));
+
+// index.set("views", path.join(__dirname, "views"));
+// index.engine(".hbs", exphbs({
+//     extname: ".hbs",
+//     defaultLayout: false,
+//     helpers: require("./helpers")
+// }));
+// index.set("view engine", ".hbs");
+  
+  
+
+// var port = process.env.PORT || 8080;
+// index.listen(port);
+// console.log("Express started. Listening on port %s", port);
