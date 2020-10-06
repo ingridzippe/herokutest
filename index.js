7357cde95c446d6210d035d7ca1644f331c1fe24
Ingrid Zippe <ingridzippe@Ingrids-MacBook-Pro.local>
2020-10-06 00:12:42 -0400

    hello

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express')
+// var index = express();
+const exphbs = require("express-handlebars");
 const path = require('path')
+const bodyParser = require("body-parser");
+
 const PORT = process.env.PORT || 5000
 
 express()
+  .use(bodyParser.urlencoded({extended: true}))
+  .use(bodyParser.json())
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
+  .engine(".hbs", exphbs({
+    extname: ".hbs",
+    defaultLayout: false,
+  }))
   .set('view engine', 'hbs')
   .get('/', (req, res) => res.render('home'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
 
 
 
