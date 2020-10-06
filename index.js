0e90c437b4ff1dd9fa635ee78b7b318d0c076bb4
Ingrid Zippe <ingridzippe@Ingrids-MacBook-Pro.local>
2020-10-06 00:19:27 -0400

    hello

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
 const express = require('express')
-// var index = express();
-const exphbs = require("express-handlebars");
+const index = express()
 const path = require('path')
-const bodyParser = require("body-parser");
-
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(bodyParser.urlencoded({extended: true}))
-  .use(bodyParser.json())
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .engine(".hbs", exphbs({
-    extname: ".hbs",
-    defaultLayout: false,
-  }))
-  .set('view engine', 'hbs')
-  .get('/', (req, res) => res.render('home'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+index.use(express.static(path.join(__dirname, 'public')))
+index.set('views', path.join(__dirname, 'views'))
+index.set('view engine', 'ejs')
+index.get('/', (req, res) => res.send("home"))
+index.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
+//   require('dotenv').config();
+// const nodemailer = require("nodemailer");
+// var express = require("express");
+// var index = express();
+// var projects = require("./projects.json");
+// var exphbs = require("express-handlebars");
+// var path = require("path");
+// var bodyParser = require("body-parser");
+
+// index.use(bodyParser.urlencoded({extended: true}));
+// index.use(bodyParser.json());
+
+// index.use(express.static(path.join(__dirname, "/public")));
+
+// index.set("views", path.join(__dirname, "views"));
+// index.engine(".hbs", exphbs({
+//     extname: ".hbs",
+//     defaultLayout: false,
+//     helpers: require("./helpers")
+// }));
+// index.set("view engine", ".hbs");
 
 
 // index.use(bodyParser.urlencoded({extended: true}));
